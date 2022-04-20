ede8c48ff9d6b7b75d7c79e3a95f1a3dc5b71be5
KarnaShiva <karna.shiva@rubrik.com>
2022-04-20 17:54:38 +0530

    Orer item and Pricebookentry Object and OrderAlignment Changes

--- force-app/main/default/lwc/availableProducts/availableProducts.js
+++ force-app/main/default/lwc/availableProducts/availableProducts.js
@@ -1,11 +1,11 @@
 /*
- Name: AvailableProducts JS Controller
- Created by: Shiva Karna
- Date: 18-Apr-2022
- Description: This controller is used to show existing order products
- and products which associated with order pricebook in order record page.
- This JS will call apex method(upsertOrderItems) to add new products in order products object.
- Changes:
+Name: AvailableProducts JS Controller
+Created by: Shiva Karna
+Date: 18-Apr-2022
+Description: This controller is used to show existing order products
+and products which associated with order pricebook in order record page.
+This JS will call apex method(upsertOrderItems) to add new products in order products object.
+Changes:
 */
 import { LightningElement, track, wire, api } from 'lwc';
 import retriveProducts from '@salesforce/apex/AvailableProductsHandler.getProducts';//retrive order products
@@ -28,160 +28,159 @@ import { publish, MessageContext } from 'lightning/messageService';
 import orderrefreshpublisher from '@salesforce/messageChannel/RefreshOrderProducts__c';
 //product object fields
 const columns = [
-    { label: 'ProductCode', fieldName: 'ProductCode',sortable: "true"},
-    { label: 'List Price', fieldName: 'UnitPrice', type: 'currency',sortable: "true" },    
+{ label: 'Name', fieldName: 'Product_Name__c',sortable: "true"},
+{ label: 'List Price', fieldName: 'UnitPrice', type: 'currency',sortable: "true" },    
 ];
 //order object columns
 const orderfields = [
-    'Order.Name',
-    'Order.Status',    
+'Order.Name',
+'Order.Status',    
 ];
 
 export default class ApexWireMethodWithParams extends LightningElement {
-    @wire(MessageContext)
-    messageContext;
-    @api recordId;
-    columns = columns;   
-    @track sortBy;
-    @track sortDirection;
-    @track record;
-    @track error;
-    @track orderstatusflag=true;
-    @track page = 1; 
-    @track startingRecord = 1;
-    @track endingRecord = 0; 
-    @track pageSize = 8; 
-    @track totalRecountCount = 0;
-    @track totalPage = 0;    
-    @track items = []; 
-    @track data = []; 
-    isPageChanged = false;    
-    @wire(retriveProducts, { orderRecID: '$recordId'})
-    products({error, data}){	      
-        if(data){            
-           this.processRecords(data);                    
-       }else if(error){
-           this.error = error;  
-       }
-   }          
-    @wire(getRecord, { recordId: '$recordId', fields: orderfields })
-    order({error, data}){	      
-         if(data){            
-            this.record = data;            
-            if(this.record.fields.Status.value == OrderStatusActivate){
-                this.orderstatusflag=false;                
-            }
-        }else if(error){
-            this.error = error;  
-        }
+@wire(MessageContext)
+messageContext;
+@api recordId;
+columns = columns;   
+@track sortBy;
+@track sortDirection;
+@track record;
+@track error;
+@track orderstatusflag=true;
+@track page = 1; 
+@track startingRecord = 1;
+@track endingRecord = 0; 
+@track pageSize = 8; 
+@track totalRecountCount = 0;
+@track totalPage = 0;    
+@track items = []; 
+@track data = []; 
+isPageChanged = false;    
+@wire(retriveProducts, { orderRecID: '$recordId'})
+products({error, data}){	      
+    if(data){            
+        this.processRecords(data);                    
+    }else if(error){
+        this.error = error;  
     }
-    //process retrived products
+}          
+@wire(getRecord, { recordId: '$recordId', fields: orderfields })
+order({error, data}){	      
+        if(data){            
+        this.record = data;            
+        if(this.record.fields.Status.value == OrderStatusActivate){
+            this.orderstatusflag=false;                
+        }
+    }else if(error){
+        this.error = error;  
+    }
+}
+//process retrived products
 processRecords(data){
-    this.items = data;
-        this.totalRecountCount = data.length; 
-        this.totalPage = Math.ceil(this.totalRecountCount / this.pageSize); 
-        
-        this.data = this.items.slice(0,this.pageSize); 
-        this.endingRecord = this.pageSize;
-        this.columns = columns;
+this.items = data;
+    this.totalRecountCount = data.length; 
+    this.totalPage = Math.ceil(this.totalRecountCount / this.pageSize); 
+    
+    this.data = this.items.slice(0,this.pageSize); 
+    this.endingRecord = this.pageSize;
+    this.columns = columns;
 }
 
-    getSelectedRec(event) {
-            var selectedRecords =  this.template.querySelector("lightning-datatable").getSelectedRows();
-            if(selectedRecords.length > 0){
-                       
-                let ids = '';
-                selectedRecords.forEach(currentItem => {
-                    ids = ids + ',' + currentItem.Product2Id;
-                });
-                this.selectedIds = ids.replace(/^,/, '');
-                this.lstSelectedRecords = selectedRecords;               
-                
-                addAndUpdateOrderItems({orderRecID :this.orderRecID ,productids : selectedRecords})
-                .then(result => {
-                this.message = result; 
-
-                if(result==successMsg){
-                    const evt = new ShowToastEvent({title: prodaddedSuccessfully,
-                        message: prodaddedSuccessfully,
-                        variant: successMsg,
-                        mode: dismissable
-                    })
-                    this.dispatchEvent(evt);     
+getSelectedRec(event) {
+        var selectedRecords =  this.template.querySelector("lightning-datatable").getSelectedRows();
+        if(selectedRecords.length > 0){
                     
-                   const message = {
-                    recordId: this.searchKey,
-                    message : lwcCompMessage,
-                    source: lwcplace,
-                    recordData: {name: LWCNameMessage}
-                };
-                publish(this.messageContext, orderrefreshpublisher, message);              
-                }else{
-                    //Show toast message with error details
-                    const evt = new ShowToastEvent({
-                        title: orderissue,
-                        message: result,
-                        variant: ErrorVariant,
-                        mode: dismissable
-                    })
-                    this.dispatchEvent(evt);   
-                }
-               //after clicking - Add Selected Products buttoon, clear the selected checkboxes                
-               this.template.querySelector('lightning-datatable').selectedRows=[];
-                // Refresh the view of the data                    
-                return refreshApex(this.products);
+            let ids = '';
+            selectedRecords.forEach(currentItem => {
+                ids = ids + ',' + currentItem.Product2Id;
+            });
+            this.selectedIds = ids.replace(/^,/, '');
+            this.lstSelectedRecords = selectedRecords;               
+            
+            addAndUpdateOrderItems({orderRecID :this.orderRecID ,productids : selectedRecords})
+            .then(result => {
+            this.message = result; 
+
+            if(result==successMsg){
+                const evt = new ShowToastEvent({title: prodaddedSuccessfully,
+                    message: prodaddedSuccessfully,
+                    variant: successMsg,
+                    mode: dismissable
                 })
-                .catch(error => {
-                this.error = error.message;
-        });              
-    } 
+                this.dispatchEvent(evt);     
+                
+                const message = {
+                recordId: this.searchKey,
+                message : lwcCompMessage,
+                source: lwcplace,
+                recordData: {name: LWCNameMessage}
+            };
+            publish(this.messageContext, orderrefreshpublisher, message);              
+            }else{
+                //Show toast message with error details
+                const evt = new ShowToastEvent({
+                    title: orderissue,
+                    message: result,
+                    variant: ErrorVariant,
+                    mode: dismissable
+                })
+                this.dispatchEvent(evt);   
+            }
+            //after clicking - Add Selected Products buttoon, clear the selected checkboxes                
+            this.template.querySelector('lightning-datatable').selectedRows=[];
+            // Refresh the view of the data                    
+            return refreshApex(this.products);
+            })
+            .catch(error => {
+            this.error = error.message;
+    });              
+} 
 }   
 //clicking on previous button this method will be called
 previousHandler() {
-    this.isPageChanged = true;
-    if (this.page > 1) {
-        this.page = this.page - 1; //decrease page by 1
-        this.displayRecordPerPage(this.page);
+this.isPageChanged = true;
+if (this.page > 1) {
+    this.page = this.page - 1; //decrease page by 1
+    this.displayRecordPerPage(this.page);
+}
+    var selectedIds = [];
+    for(var i=0; i<this.allSelectedRows.length;i++){
+    selectedIds.push(this.allSelectedRows[i].Id);
     }
-      var selectedIds = [];
-      for(var i=0; i<this.allSelectedRows.length;i++){
-        selectedIds.push(this.allSelectedRows[i].Id);
-      }
-    this.template.querySelector(
-        '[data-id="table"]'
-      ).selectedRows = selectedIds;
+this.template.querySelector(
+    '[data-id="table"]'
+    ).selectedRows = selectedIds;
 }
 
 //clicking on next button this method will be called
 nextHandler() {
-    this.isPageChanged = true;
-    if((this.page<this.totalPage) && this.page !== this.totalPage){
-        this.page = this.page + 1; //increase page by 1
-        this.displayRecordPerPage(this.page);            
-    }
-      var selectedIds = [];
-      for(var i=0; i<this.allSelectedRows.length;i++){
-        selectedIds.push(this.allSelectedRows[i].Id);
-      }
-    this.template.querySelector(
-        '[data-id="table"]'
-      ).selectedRows = selectedIds;
+this.isPageChanged = true;
+if((this.page<this.totalPage) && this.page !== this.totalPage){
+    this.page = this.page + 1; //increase page by 1
+    this.displayRecordPerPage(this.page);            
 }
- //this method displays records page by page
- displayRecordPerPage(page){
+    var selectedIds = [];
+    for(var i=0; i<this.allSelectedRows.length;i++){
+    selectedIds.push(this.allSelectedRows[i].Id);
+    }
+this.template.querySelector(
+    '[data-id="table"]'
+    ).selectedRows = selectedIds;
+}
+//this method displays records page by page
+displayRecordPerPage(page){
 
-    this.startingRecord = ((page -1) * this.pageSize) ;
-    this.endingRecord = (this.pageSize * page);
+this.startingRecord = ((page -1) * this.pageSize) ;
+this.endingRecord = (this.pageSize * page);
 
-    this.endingRecord = (this.endingRecord > this.totalRecountCount) 
-                        ? this.totalRecountCount : this.endingRecord; 
+this.endingRecord = (this.endingRecord > this.totalRecountCount) 
+                    ? this.totalRecountCount : this.endingRecord; 
 
-    this.data = this.items.slice(this.startingRecord, this.endingRecord);
-    this.startingRecord = this.startingRecord + 1;
+this.data = this.items.slice(this.startingRecord, this.endingRecord);
+this.startingRecord = this.startingRecord + 1;
 } 
 //connectedCallback function is similar to init method in Lightning Components.
-    connectedCallback(){
-        this.orderRecID = this.recordId;        
-    }   
-
+connectedCallback(){
+    this.orderRecID = this.recordId;        
+}   
 }
--- force-app/main/default/lwc/orderedProducts/orderedProducts.js
+++ force-app/main/default/lwc/orderedProducts/orderedProducts.js
@@ -1,10 +1,10 @@
 /*
- Name: OrderedProducts JS Controller
- Created by: Shiva Karna
- Date: 18-Apr-2022
- Description: This controller is used to show existing order products in order record page.
- This updates order status to Activated
- Changes:
+Name: OrderedProducts JS Controller
+Created by: Shiva Karna
+Date: 18-Apr-2022
+Description: This controller is used to show existing order products in order record page.
+This updates order status to Activated
+Changes:
 */
 import { LightningElement, track, wire, api } from 'lwc';
 import retriveOrderProducts from '@salesforce/apex/OrderProductsHandler.getOrderProducts';//import order products from apex
@@ -24,127 +24,127 @@ import OrderUpdated from '@salesforce/label/c.OrderUpdated';//custom label
 
 
 import {
-    subscribe,
-    unsubscribe,
-    APPLICATION_SCOPE,
-    MessageContext
-  } from "lightning/messageService";
+subscribe,
+unsubscribe,
+APPLICATION_SCOPE,
+MessageContext
+} from "lightning/messageService";
 import SAMPLEMC from "@salesforce/messageChannel/RefreshOrderProducts__c";
 
 //order item columns
 const columns = [    
-    { label: 'Name', fieldName: 'Product_Code__c',sortable: "true"},
-    { label: 'Unit Price', fieldName: 'UnitPrice', type: 'currency',sortable: "true" },
-    { label: 'Total Price', fieldName: 'TotalPrice', type: 'currency',sortable: "true"},  
-    { label: 'Quantity', fieldName: 'Quantity', type: 'number',sortable: "true"},  
+{ label: 'Name', fieldName: 'Product_Name__c',sortable: "true"},
+{ label: 'Unit Price', fieldName: 'UnitPrice', type: 'currency',sortable: "true" },
+{ label: 'Quantity', fieldName: 'Quantity', type: 'number',sortable: "true"}, 
+{ label: 'Total Price', fieldName: 'TotalPrice', type: 'currency',sortable: "true"},       
 ];
 //order object columns
 const orderfields = [
-    'Order.Name',
-    'Order.Status',    
+'Order.Name',
+'Order.Status',    
 ];
 
 export default class ApexWireMethodWithParams extends LightningElement {
 
-    @wire(MessageContext)
-  messageContext;
+@wire(MessageContext)
+messageContext;
 
-  subscription = null;
-  receivedMessage;
-  isDisabled = false;
-  isDisabledUnsb = true;
+subscription = null;
+receivedMessage;
+isDisabled = false;
+isDisabledUnsb = true;
 
-    @api recordId;
-    @api orderitems1;
-    columns = columns;   
-    @track sortBy;
-    @track sortDirection;   
-    @track record;
-    @track error;
-    @track orderstatusflag=true;
+@api recordId;
+@api orderitems1;
+columns = columns;   
+@track sortBy;
+@track sortDirection;   
+@track record;
+@track error;
+@track orderstatusflag=true;
 
-    @wire(retriveOrderProducts, { orderRecID: '$recordId'})
-    orderitems;    
-    @wire(getRecord, { recordId: '$recordId', fields: orderfields })
-    order({error, data}){	      
-         if(data){            
-            this.record = data;//const paramField1 = getFieldValue(data, orderStatus_Fld);              
-            if(this.record.fields.Status.value == OrderStatusActivate){
-                this.orderstatusflag=false;                
-            }
-        }else if(error){
-            this.error = error;  
+@wire(retriveOrderProducts, { orderRecID: '$recordId'})
+orderitems;    
+@wire(getRecord, { recordId: '$recordId', fields: orderfields })
+order({error, data}){	      
+        if(data){            
+        this.record = data;//const paramField1 = getFieldValue(data, orderStatus_Fld);              
+        if(this.record.fields.Status.value == OrderStatusActivate){
+            this.orderstatusflag=false;                
         }
+    }else if(error){
+        this.error = error;  
     }
+}
 
-    handleMessage(message) {
-        refreshApex(this.orderitems);       
-    }   
-    handleSortAccountData(event) {       
-        this.sortBy = event.detail.fieldName;       
-        this.sortDirection = event.detail.sortDirection;       
-        this.sortOrderProducts(event.detail.fieldName, event.detail.sortDirection);
-    }
-    sortOrderProducts(fieldname, direction) {
-        
-        let parseData = JSON.parse(JSON.stringify(this.orderitems.data));       
-        let keyValue = (a) => {
-            return a[fieldname];
-        };
-       let isReverse = direction === 'asc' ? 1: -1;
-           parseData.sort((x, y) => {
-            x = keyValue(x) ? keyValue(x) : ''; 
-            y = keyValue(y) ? keyValue(y) : '';           
-            return isReverse * ((x > y) - (y > x));
-        });        
-        this.orderitems.data = parseData;
-        }
-
+handleMessage(message) {
+    refreshApex(this.orderitems);       
+}   
+handleSortAccountData(event) {       
+    this.sortBy = event.detail.fieldName;       
+    this.sortDirection = event.detail.sortDirection;       
+    this.sortOrderProducts(event.detail.fieldName, event.detail.sortDirection);
+}
+sortOrderProducts(fieldname, direction) {
     
+    let parseData = JSON.parse(JSON.stringify(this.orderitems.data));       
+    let keyValue = (a) => {
+        return a[fieldname];
+    };
+    let isReverse = direction === 'asc' ? 1: -1;
+        parseData.sort((x, y) => {
+        x = keyValue(x) ? keyValue(x) : ''; 
+        y = keyValue(y) ? keyValue(y) : '';           
+        return isReverse * ((x > y) - (y > x));
+    });        
+    this.orderitems.data = parseData;
+    }
+
+
 //connectedCallback function is similar to init method in Lightning Components.
-    connectedCallback(){
-        this.searchKey = this.recordId;        
-        this.isDisabled = true;
-        this.isDisabledUnsb = false;        
-        if (this.subscription) {
-            return;
-        }
-        this.subscription = subscribe(
-        this.messageContext,
-        SAMPLEMC,
-        message => {
-        this.handleMessage(message);
-        },
-        { scope: APPLICATION_SCOPE }
-        );
-    }    
+connectedCallback(){
+    this.searchKey = this.recordId;        
+    this.isDisabled = true;
+    this.isDisabledUnsb = false;        
+    if (this.subscription) {
+        return;
+    }
+    this.subscription = subscribe(
+    this.messageContext,
+    SAMPLEMC,
+    message => {
+    this.handleMessage(message);
+    },
+    { scope: APPLICATION_SCOPE }
+    );
+}    
 
-    handleActivateOrderClick(event) {       
-        updateOrderStatusJS({ orderRecID: this.recordId})
-        .then(result =>{
-            if(result==successMsg){
-                const evt = new ShowToastEvent({
-                    title: OrderUpdated,
-                    message: OrderActivatedSuccessfully,
-                    variant: successMsg,
-                    mode: dismissable
-                })
-                this.dispatchEvent(evt);
-                //eval("$A.get('e.force:refreshView').fire();");
+handleActivateOrderClick(event) {       
+    updateOrderStatusJS({ orderRecID: this.recordId})
+    .then(result =>{
+        if(result==successMsg){
+            const evt = new ShowToastEvent({
+                title: OrderUpdated,
+                message: OrderActivatedSuccessfully,
+                variant: successMsg,
+                mode: dismissable
+            })
+            this.dispatchEvent(evt);
+            eval("$A.get('e.force:refreshView').fire();");
 
-            }else{
-                const evt = new ShowToastEvent({
-                    title: orderissue,
-                    message: result,
-                    variant: ErrorVariant,
-                    mode: dismissable
-                })
-                this.dispatchEvent(evt);   
-            }                    
-        }).catch(error => {
-            this.error = error;
-           
-        });;
-     }
+        }else{
+            const evt = new ShowToastEvent({
+                title: orderissue,
+                message: result,
+                variant: ErrorVariant,
+                mode: dismissable
+            })
+            this.dispatchEvent(evt);   
+        }                    
+    }).catch(error => {
+        this.error = error;
+        
+    });;
+    }
 
 }
